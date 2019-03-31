1b11875bea26f4df4faacdc3ae0d601458ff48db
Xiaohai Xu <xiaohaix@student.unimelb.edu.au>
2019-03-31 16:10:17 +1100

    search 2d matrix II

---
+++ 240.cpp
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/search-a-2d-matrix-ii/
+/*
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int m = matrix.size();
+        if (!m) return false;
+        int n = matrix[0].size();
+        if (!n) return false;
+        int row = 0;
+        int col = n - 1;
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+};
+
+/*
+Runtime: 64 ms, faster than 97.09% of C++ online submissions for Search a 2D Matrix II.
+Memory Usage: 12.8 MB, less than 86.04% of C++ online submissions for Search a 2D Matrix II.
+*/
